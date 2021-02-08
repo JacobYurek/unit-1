fc93978d122e745ab363cbe6252654fb4d92e635
JacobYurek <78129492+JacobYurek@users.noreply.github.com>
2021-02-07 23:45:10 -0600

    Update main_with_debug.js

--- js/main_with_debug.js
+++ js/main_with_debug.js
@@ -41,61 +41,69 @@ function cities(){
         //add the row's html string to the table
         $("table").append(rowHtml);
     };
-
+    //calls on the function addColumns defined below with the parameter of the cityPop table
     addColumns(cityPop);
+    //calls on the function addEvents defined below
     addEvents();
 };
-
+//function to add a new column to the cityPop table
 function addColumns(cityPop){
-    
+    //each method to loop through the table with a function
     $('tr').each(function(i){
+    	//first if else statement for when number of loops is 0
     	if (i == 0){
-
+    		//append to create a column header of city size
     		$(this).append('<th>City Size</th>');
+    	//else for when the loop is greater than 1
     	} else {
+    		//creates a variable citySize
     		var citySize;
-
+    		//second if else statement that compares the cityPop value to 1000000 and sets its citySize value to small if lesser
     		if (cityPop[i-1].population < 100000){
     			citySize = 'Small';
-
+    		//else if statement that compares the cityPop value to 5000000 and sets its citySize value to medium if lesser
     		} else if (cityPop[i-1].population < 500000){
     			citySize = 'Medium';
-
+    		//else statement that compares the cityPop value to 5000000 and sets its citySize value to medium if lesser
     		} else {
     			citySize = 'Large';
     		};
-
+    		//add the citySize value to the table
     		$(this).append('<td>' + citySize + '</td>');
     	};
     });
 };
-
+//function to add color mouseover function
 function addEvents(){
-
+	//creates a mouseover function for when cursor is over the table 
 	$('table').mouseover(function(){
+		//sets variable color to be string
 		var color = "rgb(";
-
+		//for loop 
 		for (var i=0; i<3; i++){
-
+			//creates random number and multiplies it by 255 then assigns it to a variable random
 			var random = Math.round(Math.random() * 255);
-
+			//addition assignment statement to add the variable random to the variable color
 			color += random;
-
+			//if else statement to determine location in the for loop
 			if (i<2){
+				//addition assignment statement to string to color variable
 				color += ",";
-			
+			//else statement for the end of the for loop
 			} else {
+				//addition assignment statement to string to color variable
 				color += ")";
 		};
-
+		//alters the css color to include the color variable we created in function
 		$(this).css('color', color);
 	};
 });
+	//creates a function for on page clicks
 	function clickme(){
-
+		//method that will create an alert box to display string
 		alert('Hey, you clicked me!');
 	};
-
+	//uses on method to create and event listener with the event in this case being a mouse click on table
 	$('table').on('click', clickme);
 };
 
